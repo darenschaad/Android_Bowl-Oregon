5215b6a6554a2fac29239bbcf97789bd87791ba2
Daren Schaad <darenschaad@gmail.com>
2016-04-22 11:01:32 -0700

    Add layout to ScoresActivity

--- app/src/main/java/com/epicodus/bowloregon/MainActivity.java
+++ app/src/main/java/com/epicodus/bowloregon/MainActivity.java
@@ -1,13 +1,43 @@
 package com.epicodus.bowloregon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    @Bind(R.id.buttonScores) Button mButtonScores;
+    @Bind(R.id.buttonStats) Button mButtonStats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mButtonScores.setOnClickListener(this);
+        mButtonStats.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonStats:
+                Intent intent = new Intent(MainActivity.this, StatsActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.buttonScores:
+                Intent intent1 = new Intent(MainActivity.this, ScoresActivity.class);
+                startActivity(intent1);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
