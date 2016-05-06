0f0be3c665a8fbd96476766cc183d0962ee473fe
Daren Schaad <darenschaad@gmail.com>
2016-05-06 08:26:19 -0700

    Adding user Auth

--- app/src/main/java/com/epicodus/bowloregon/Constants.java
+++ app/src/main/java/com/epicodus/bowloregon/Constants.java
@@ -17,4 +17,11 @@ public class Constants {
 
     public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=bowling%20alley";
     public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
+
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_PROPERTY_EMAIL = "email";
+    public static final String KEY_UID = "UID";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
 }
